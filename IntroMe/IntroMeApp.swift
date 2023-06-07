c2689d841ba3598a62ef0e635c8137326388d80b
Rodion <rodion.kuskov@gmail.com>
2023-06-07 16:57:37 +0300

    Initial Commit

---
+++ IntroMe/IntroMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroMeApp.swift
+//  IntroMe
+//
+//  Created by Rodion Kuskov on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct IntroMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
